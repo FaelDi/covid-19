1cab4840fcdb991eace858b69f42c0b6f4274b2b
faeldii@icloud.com <faeldii@icloud.com>
2020-03-24 11:56:48 -0300

    Initial Commit

---
+++ covid-19/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  covid-19
+//
+//  Created by Rafael Dias ferreira on 24/03/20.
+//  Copyright © 2020 Rafael Dias ferreira. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ covid-19/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  covid-19
+//
+//  Created by Rafael Dias ferreira on 24/03/20.
+//  Copyright © 2020 Rafael Dias ferreira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
